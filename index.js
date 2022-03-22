8affa42b12819eeee8708cc6215b981bc8d357a7
diegofreitas50 <diegofreitas50@hotmail.com>
2022-03-22 18:44:29 -0300

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const { dirname } = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT  || 3000;
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
