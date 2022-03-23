fef159585b9eb32676fa08c060f106d1ca0c3fe1
diegofreitas50 <diegofreitas50@hotmail.com>
2022-03-23 14:57:28 -0300

    detalhes

--- index.js
+++ index.js
@@ -12,25 +12,40 @@ app.use(express.urlencoded());
 const pokedex = [
     {
         id: 1,
+        numero: "001",
         nome: "Bulbasaur",
         descricao: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
         tipo: "Grass",
+        altura: "0.7 m",
+        peso: "6.9 kg",
+        categoria: "Seed",
+        habilidade: "Overgrow",
         imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
     },
 
     {
         id: 2,
+        numero: "004",
         nome: "Charmander",
         descricao: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
         tipo: "Fire",
+        altura: "0.6 m",
+        peso: "8.5 kg",
+        categoria: "Lizard",
+        habilidade: "Blase",
         imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
     },
 
     {
         id: 3,
+        numero: "007",
         nome: "Squirtle",
         descricao: "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
         tipo: "Water",
+        altura: "0.5 m",
+        peso: "9.0 kg",
+        categoria: "Tiny Turtle",
+        habilidade: "Torrent",
         imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
     }
     
@@ -60,7 +75,7 @@ app.post("/create", (req, res) => {
 app.get("/editar/:id", (req, res) => {
     const id = +req.params.id;
     pokemon = pokedex.find((pokemon) => pokemon.id === id);
-    res.redirect("/#cadastro");
+    res.redirect("/cadastro");
 });
 
 app.post("/update/:id", (req, res) => {
